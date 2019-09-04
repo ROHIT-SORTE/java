1f1d234beb861147447d3836a5531b361e4fa485
ROHIT-SORTE <rsorte22@gmail.com>
2019-09-04 07:08:11 +0530

    core java

---
+++ core_java/Day_1/Some_Basic_Programs/Do_While_Demo.java
@@ -0,0 +1,15 @@
+package datatypes_pro;
+
+public class Do_While_Demo
+{
+	public static void main(String[] args)
+	{
+		int a=10;
+		// Post-Test Loop
+		do
+		{
+			System.out.println(a);
+			a++;
+		}while(a<20);
+	}
+}
---
+++ core_java/Day_1/Some_Basic_Programs/If_Demo.java
@@ -0,0 +1,21 @@
+package datatypes_pro;
+
+public class If_Demo
+{
+	public static void main(String[] args)
+	{
+		int a=10;
+		if(a>0)
+		{
+			System.out.println("it's positive");
+		}
+		else if(a<0)
+		{
+			System.out.println("it's negative");
+		}
+		else
+		{
+			System.out.println("it's negative");
+		}
+	}
+}
